4f5c75a584f1aab29b87616c786ac5ff9090ecf2
Barogthor <f.thery3103@gmail.com>
2019-08-22 01:20:41 +0200

    log debug

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,1 @@
-pub mod slk;
 pub mod map_data;
--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,38 @@
-use std::fs::File;
-use byteorder::{ReadBytesExt, LittleEndian, BigEndian};
-use std::ffi::{CString, CStr, NulError};
-use std::io::{Seek, SeekFrom, Read};
-use std::error::Error;
+#[macro_use] extern crate lazy_static;
+extern crate regex;
+
 use war_editor::map_data::w3i_file::W3iFile;
-//use byteorder::{LittleEndian,BigEndian,ReadBytesExt};
-//use std::io::Cursor;
-
-struct Unit {
-}
-
-fn read_w3u_file(){
-    let mut f = File::open("resources/war3map.w3u").unwrap();
-    let version = f.read_i32::<LittleEndian>().unwrap();
-    let num_origin_units = f.read_i32::<LittleEndian>().unwrap();
-    let num_custom_units = f.read_i32::<LittleEndian>().unwrap();
-    println!("version {}", version);
-    println!("num origin units {}", num_origin_units);
-    println!("num origin units {}", num_custom_units);
-}
+use std::mem::size_of_val;
+use war_editor::slk::slk::SLKReader;
+use war_editor::map_data::mmp_file::MMPFile;
+use war_editor::map_data::region_file::RegionFile;
+use war_editor::map_data::camera_file::{CameraFile};
+use war_editor::map_data::sound_file::SoundFile;
+use war_editor::map_data::pathmap_file::PathMapFile;
+use war_editor::map_data::shadowmap_file::ShadowMapFile;
+use war_editor::map_data::environment_file::EnvironmentFile;
 
 
 fn main() {
     println!("Hello, world!");
-    W3iFile::read_file();
-
+    let w3i = W3iFile::read_file();
+    w3i.debug();
+//    println!("size w3i: {}",size_of_val(&w3i));
+//    let mut slk_reader = SLKReader::open_file("resources/slk/Cliffs.slk".to_string());
+//    let record = slk_reader.parse();
+//    println!("{:#?}",record);
+    let mmp = MMPFile::read_file();
+//    mmp.debug();
+    let regions = RegionFile::read_file();
+//    regions.debug();
+    let cameras = CameraFile::read_file();
+//    cameras.debug();
+//    let sounds = SoundFile::read_file();
+//    sounds.debug();
+    let pathing = PathMapFile::read_file();
+//    pathing.debug();
+    let shaders = ShadowMapFile::read_file();
+//    shaders.debug();
+    let environment = EnvironmentFile::read_file();
+    environment.debug();
 }
