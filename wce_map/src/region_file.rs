fcc091808f3ef2999d003d38c806388533c910c7
Barogthor <f.thery3103@gmail.com>
2020-04-08 00:37:11 +0200

    region file: add test

--- wce_map/src/region_file.rs
+++ wce_map/src/region_file.rs
@@ -1,4 +1,5 @@
-use std::ffi::CString;
+#[cfg(test)]
+use pretty_assertions::{assert_eq, assert_ne};
 
 use mpq::Archive;
 
@@ -6,44 +7,29 @@ use crate::globals::MAP_REGIONS;
 use crate::binary_reader::{BinaryConverter, BinaryReader};
 use crate::binary_writer::BinaryWriter;
 
-#[derive(Debug)]
+#[derive(Debug, Derivative)]
+#[derivative(Default, PartialEq)]
 pub struct Region {
     left: f32,
     right: f32,
     bottom: f32,
     top: f32,
-    name: CString,
+    name: String,
     index: u32,
     weather_effect: String,
     weather_enabled: bool,
-    ambient_sound: CString,
+    ambient_sound: String,
     color: Vec<u8>,
     // skip 1 byte : end structure
-}
-impl Default for Region{
-    fn default() -> Self {
-        Region{
-            left: 0.0,
-            right: 0.0,
-            bottom: 0.0,
-            top: 0.0,
-            name: Default::default(),
-            index: 0,
-            weather_effect: "".to_string(),
-            weather_enabled: false,
-            ambient_sound: Default::default(),
-            color: vec![]
-        }
-    }
 }
 impl BinaryConverter for Region{
     fn read(reader: &mut BinaryReader) -> Self {
         let mut region = Self::default();
         region.left = reader.read_f32();
-        region.right = reader.read_f32();
         region.bottom = reader.read_f32();
+        region.right = reader.read_f32();
         region.top = reader.read_f32();
-        region.name = reader.read_c_string();
+        region.name = reader.read_c_string().into_string().unwrap();
         region.index = reader.read_u32();
 //        let effect_id = reader.read_bytes(4);
 //        region.weather_effect = String::from_utf8(effect_id).unwrap();
@@ -51,7 +37,7 @@ impl BinaryConverter for Region{
         if region.weather_effect.as_bytes() == [0u8;4] {
             region.weather_enabled = false;
         }
-        region.ambient_sound = reader.read_c_string();
+        region.ambient_sound = reader.read_c_string().into_string().unwrap();
         region.color = reader.read_bytes(3);
         reader.skip(1);
         region
@@ -103,5 +89,69 @@ impl BinaryConverter for RegionFile{
 
     fn write(&self, _writer: &mut BinaryWriter) {
         unimplemented!()
+    }
+}
+
+#[cfg(test)]
+mod w3r_test{
+    use std::fs::File;
+    use crate::binary_reader::BinaryReader;
+    use crate::region_file::{RegionFile, Region};
+
+    fn mock_regions() -> Vec<Region>{
+        vec![
+            Region{
+                left: -832.0,
+                right: -480.0,
+                bottom: -640.0,
+                top: -256.0,
+                name: "Red".to_string(),
+                index: 0,
+                weather_effect: "RAhr".to_string(),
+                weather_enabled: false,
+                ambient_sound: "gg_snd_RainAmbience".to_string(),
+                color: vec![0, 0, 255]
+            },
+            Region{
+                left: 416.0,
+                right: 768.0,
+                bottom: -32.0,
+                top: 352.0,
+                name: "LightGreen".to_string(),
+                index: 1,
+                weather_effect: "\0\0\0\0".to_string(),
+                weather_enabled: false,
+                ambient_sound: "gg_snd_Avatar".to_string(),
+                color: vec![128, 255, 128]
+            },
+            Region{
+                left: 384.0,
+                right: 416.0,
+                bottom: -1056.0,
+                top: -640.0,
+                name: "White".to_string(),
+                index: 2,
+                weather_effect: "\0\0\0\0".to_string(),
+                weather_enabled: false,
+                ambient_sound: "".to_string(),
+                color: vec![255, 255, 255]
+            }
+        ]
+    }
+
+    #[test]
+    fn no_failure(){
+        let mut w3r = File::open("../resources/Scenario/Sandbox_roc/war3map.w3r").unwrap();
+        let mut reader = BinaryReader::from(&mut w3r);
+        reader.read::<RegionFile>();
+    }
+
+    #[test]
+    fn check_values(){
+        let mut w3r = File::open("../resources/Scenario/Sandbox_roc/war3map.w3r").unwrap();
+        let mut reader = BinaryReader::from(&mut w3r);
+        let region_file = reader.read::<RegionFile>();
+        let mock_regions = mock_regions();
+        assert_eq!(region_file.regions, mock_regions);
     }
 }
