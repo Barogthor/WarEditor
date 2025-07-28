8b65238b2415c2765221019e6ebefd58b98bccf6
Barogthor <f.thery3103@gmail.com>
2025-07-28 20:30:35 +0200

    add minimap test

--- crates/wce_map/src/minimap_file.rs
+++ crates/wce_map/src/minimap_file.rs
@@ -36,3 +36,22 @@ impl MinimapFile {
         Ok(Self { minimap })
     }
 }
+
+#[cfg(test)]
+mod minimap_test {
+    use wce_formats::MapArchive;
+
+    use crate::{get_resources_path, minimap_file::MinimapFile};
+
+    fn get_path(path_resource: &str) -> String {
+        let base_path = get_resources_path();
+        format!("{base_path}/{path_resource}")
+    }
+
+    #[test]
+    fn no_failure() {
+        let map_path = get_path("Scenario/Sandbox_1.w3m");
+        let mut map = MapArchive::open(map_path).unwrap_or_else(|e| panic!("{}", e));
+        MinimapFile::read_file(&mut map).unwrap_or_else(|e| panic!("{}", e));
+    }
+}
